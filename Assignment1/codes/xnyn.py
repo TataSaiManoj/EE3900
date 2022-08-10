24fc98d944b83ded4f43f19e5cc49026cb3b1f81
TataSaiManoj <110447588+TataSaiManoj@users.noreply.github.com>
2022-08-10 11:23:47 +0530

    updated xnyn.py

--- Assignment1/codes/xnyn.py
+++ Assignment1/codes/xnyn.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 #import shlex
 #end if
 
+# plot using C code 
+f = open("input.txt", "r")
+s = f.readline()
+res = [float(value) for value in s.split()]
+print(res)
+
 x=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
 k = 20
 y = np.zeros(20)
@@ -21,18 +27,18 @@ for n in range(2,k-1):
 	else:
 		y[n] = -0.5*y[n-1]
 		
-print(y)
+# print(y)
 
 #subplots
 plt.subplot(2, 1, 1)
-plt.stem(range(0,6),x)
+plt.stem(range(0,6),x, use_line_collection = True)
 plt.title('Digital Filter Input-Output')
 plt.ylabel('$x(n)$')
 plt.grid()# minor
 
 
 plt.subplot(2, 1, 2)
-plt.stem(range(0,k),y)
+plt.stem(range(0,k),y, use_line_collection = True)
 plt.xlabel('$n$')
 plt.ylabel('$y(n)$')
 plt.grid()# minor
@@ -43,3 +49,11 @@ plt.savefig('../figs/xnyn.eps')
 #subprocess.run(shlex.split("termux-open ../figs/xnyn.pdf"))
 #else
 #plt.show()
+
+# 3.3 subplot
+plt.stem(range(0,k),res, use_line_collection = True)
+plt.xlabel('$n$')
+plt.ylabel('$y(n)$')
+plt.grid()
+
+plt.savefig('../figs/xnyn_using_c.pdf')
